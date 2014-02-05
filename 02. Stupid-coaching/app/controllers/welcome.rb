7875312e209c2ae463f5073b696a3e2eb3e6b687
Chris <c.chriv@gmail.com>
2014-02-05 14:46:38 +0100

    Premiers pas avec Sinatra

--- 02. Stupid-coaching/app/controllers/welcome.rb
+++ 02. Stupid-coaching/app/controllers/welcome.rb
@@ -9,7 +9,25 @@ get '/' do
 end
 
 post '/coach' do
-  puts "self instance variable from POST '/coach' route handler: #{self.instance_variables}"
-  
-  "Implement your coach response ! params => #{params.inspect}"
+  puts "self instance variable from POST '/coach' route handler: #{self.instance_variables}"  
+  answer = params[:what]
+
+  	if "I'm going to work"
+	 		@coach_message = "Good boy"
+	 	elsif "I'm bored !"
+			@coach_message = "Good boy ! That's the spirit (and the answer I was waiting for)."
+	 	elsif answer.upcase
+	 		@coach_message = "I can feel your motivation son !"
+	 	elsif answer.end_with?("?")
+			@coach_message = "Silly question ! Get dressed and go to work !"
+		elsif answer.include?("fuck")
+			@coach_message = "STAY POLITE OR YOU'LL GET MY FOOT IN YOUR ASS !"
+		else
+			"You said '#{answer}' ? What the fuck ? I don't care, kid ! Get dressed and run around 100 times !"
+	end
+	erb :index
 end
+
+
+
+
